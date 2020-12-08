163ea16612df8c85f21ec0868ccf157f0afe9b69
Emil Djupvik <emild@Emils-MacBook-Air.local>
2020-12-08 15:08:55 +0100

    add comments

--- vector_field.py
+++ vector_field.py
@@ -6,6 +6,15 @@ from particle import *
 
 
 class Vector_Field:
+    '''
+    Stores a field such that each (x, y) position in the field
+    corresponds with a vector given by function
+
+        f(pos: Vector): Vector
+
+    This object also manages an array of particles that travels
+    in the vector field.
+    '''
     def __init__(self, screen, size: list, function):
         self.screen = screen
         self.size = size
@@ -14,6 +23,9 @@ class Vector_Field:
         self.particles = [Particle(size) for _ in range(70)]
 
     def show(self):
+        '''
+        Renders the particles
+        '''
         # w, h = self.size
 
         # space = w / 20
@@ -28,10 +40,21 @@ class Vector_Field:
             particle.show(self.screen)
 
     def update(self):
+        '''
+        Updates the particles by setting their velocities
+        to their corresponding vector in the vector field
+        '''
         for particle in self.particles:
             particle.update(self.function(particle.pos))
     
     def add_particle(self, particle):
+        '''
+            Adds a special particle to the stored particles
+
+            Raises an exception if given particle is not special
+        '''
+        if not particle.special:
+            raise Exception('please only add special particles')
         if self.particles[0].special:
             del self.particles[0] 
         self.particles.insert(0, particle)
